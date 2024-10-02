cdef5b344f1274b2494007ddc75c981507c1508b
tmspworld <gagamandeep@gmail.com>
2024-10-02 23:34:27 +0530

    L1_Variables_and_Constants.js

---
+++ JavaScript Basic/L1_Variables_and_Constants.js
@@ -0,0 +1,15 @@
+//1. Variables and Constants
+
+// In JavaScript, we use var, let, and const to declare variables.
+
+let fname = 'React Learner'; // let allows the value to be changed
+const appName = 'React';    // const value cannot be changed
+
+console.log(fname);
+console.log(appName);
+
+fname="Jay"; // This is Possible
+//appName="New React"; // This is Not possible. It will generate an error! 
+//TypeError: Assignment to constant variable.
+console.log(fname);
+console.log(appName);
